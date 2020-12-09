05012688f64af9dd91073f30131a39295890d718
Ezequiel-Ferreira <ezequielferre21@edu.unifor.br>
2020-12-09 10:08:41 -0300

    hr-worker implementation

--- hr-worker/src/main/java/br/com/ezequielprojets/hrworker/resources/WorkerResourse.java
+++ hr-worker/src/main/java/br/com/ezequielprojets/hrworker/resources/WorkerResourse.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +20,15 @@ public class WorkerResourse {
 	@Autowired
 	private WorkerRepository workerRepo;
 	
+	@GetMapping
 	public ResponseEntity<List<Worker>> getAll(){
-		
-		List<Worker> workers = new ArrayList<Worker>();
-		this.workerRepo.findAll().stream().forEach(workers::add);
-		
-		return new ResponseEntity<>(workers, HttpStatus.OK);
-		
+		List<Worker> list = new ArrayList<Worker>();
+		this.workerRepo.findAll().stream().forEach(list::add);
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> getById(@PathVariable("id") Long id){
+		Worker obj = this.workerRepo.findById(id).get();
+		return new ResponseEntity<>(obj, HttpStatus.OK);
 	}
 }
