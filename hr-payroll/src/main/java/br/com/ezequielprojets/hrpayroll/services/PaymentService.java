2d61f43137967bc2debe7e39f5b19e2de4321591
Ezequiel-Ferreira <ezequielferre21@edu.unifor.br>
2020-12-10 18:04:14 -0300

    RestTemplete

--- hr-payroll/src/main/java/br/com/ezequielprojets/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/ezequielprojets/hrpayroll/services/PaymentService.java
@@ -1,14 +1,31 @@
 package br.com.ezequielprojets.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.ezequielprojets.hrpayroll.entities.Payment;
+import br.com.ezequielprojets.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplete;
 	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+ workerId);
+
+		
+		Worker worker = this.restTemplete.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
 }
