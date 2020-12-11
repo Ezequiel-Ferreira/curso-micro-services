2010348a8738921aa5f7eab604878578a9014c57
Ezequiel-Ferreira <ezequielferre21@edu.unifor.br>
2020-12-11 17:19:42 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/br/com/ezequielprojets/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/ezequielprojets/hrpayroll/resources/PaymentResource.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.ezequielprojets.hrpayroll.entities.Payment;
 import br.com.ezequielprojets.hrpayroll.services.PaymentService;
 
@@ -18,8 +20,13 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService paymentService;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable("workerId") Long id,@PathVariable("days") Integer days ){
 		return new ResponseEntity<>(paymentService.getPayment(id, days), HttpStatus.OK);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(@PathVariable("workerId") Long id,@PathVariable("days") Integer days ){
+		return new ResponseEntity<>(new Payment("Servidor vazio", 0.0, 0), HttpStatus.OK);
 	}
 }
